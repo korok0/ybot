b4a90d3695d332a26f666cbaf0397ad2ad96252a
korok <leafkorokleaf@gmail.com>
2023-08-09 14:58:08 -0400

    removed bot case reponse in get_profile steam.py

--- src/cogs/steam.py
+++ src/cogs/steam.py
@@ -45,35 +45,31 @@ class SteamCommands(commands.Cog):
         a_color = discord.Colour.dark_theme()
         # bot cannot have profile
         view = None
-        if member.id == interaction.client.user.id:
+        # check if user is registered and if token is valid
+        if u.is_registered(member.id) and u.test_token(member.id):
+            b_token = u.fetch_token(member.id)
+            # check if user 
+            steam_id = u.get_user_steam_id(b_token)
             embed = discord.Embed(color=a_color,
-                                    title=f'Bot does not have a steam profile!')
-        else: 
-            # check if user is registered and if token is valid
-            if u.is_registered(member.id) and u.test_token(member.id):
-                b_token = u.fetch_token(member.id)
-                # check if user 
-                steam_id = u.get_user_steam_id(b_token)
-                embed = discord.Embed(color=a_color,
-                                title=f'User must add steam to their connections in **settings > connections**')
-                if steam_id is not None:
-                    p_sum_url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_KEY}&steamids="
-                    game_url = f"https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={STEAM_KEY}&steamid={steam_id}&count=1"
-                    # unpack steam data
-                    p_sum_url+=steam_id
-                    data = u.get_steam_data(p_sum_url)
-                    game_data = u.get_steam_data(game_url)
-                    avatar, name, country, profile_url, time_created = (u.unpack(0, data, 'avatarfull', 'players'), 
-                            u.unpack(0, data, 'personaname', 'players'), u.unpack(0, data, 'loccountrycode', 'players'), 
-                            u.unpack(0, data, 'profileurl', 'players'), u.unpack(0, data, 'timecreated', 'players'))
-                    if len(country) == 2: country = f":flag_{country.lower()}:"
+                            title=f'User must add steam to their connections in **settings > connections**')
+            if steam_id is not None:
+                p_sum_url = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_KEY}&steamids="
+                game_url = f"https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={STEAM_KEY}&steamid={steam_id}&count=1"
+                # unpack steam data
+                p_sum_url+=steam_id
+                data = u.get_steam_data(p_sum_url)
+                game_data = u.get_steam_data(game_url)
+                avatar, name, country, profile_url, time_created = (u.unpack(0, data, 'avatarfull', 'players'), 
+                        u.unpack(0, data, 'personaname', 'players'), u.unpack(0, data, 'loccountrycode', 'players'), 
+                        u.unpack(0, data, 'profileurl', 'players'), u.unpack(0, data, 'timecreated', 'players'))
+                if len(country) == 2: country = f":flag_{country.lower()}:"
 
-                    embed = discord.Embed(colour=a_color,title=f'{name}\'s profile', url=profile_url, description=f"**Country:** {country}").add_field(name=f"**__Created__** ", 
-                        value=f"{datetime.utcfromtimestamp(int(time_created)).strftime('%b %d %Y')}").set_thumbnail(url=avatar)
-                    view = SteamSelectMenu(interaction=interaction, embed=embed, steam_id=steam_id, data=game_data)
-            else:
-                embed = discord.Embed(color=a_color,
-                                title=f'User must register their account by using **/link** command')
+                embed = discord.Embed(colour=a_color,title=f'{name}\'s profile', url=profile_url, description=f"**Country:** {country}").add_field(name=f"**__Created__** ", 
+                    value=f"{datetime.utcfromtimestamp(int(time_created)).strftime('%b %d %Y')}").set_thumbnail(url=avatar)
+                view = SteamSelectMenu(interaction=interaction, embed=embed, steam_id=steam_id, data=game_data)
+        else:
+            embed = discord.Embed(color=a_color,
+                            title=f'User must register their account by using **/link** command')
         embed.set_author(name=member.name, icon_url=member.avatar)
         await interaction.response.send_message(embed=embed, view=view)
             
