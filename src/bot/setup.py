b1694490024a6ca232c31afe9d8dfe487eed7552
korok <leafkorokleaf@gmail.com>
2023-09-08 22:34:51 -0400

    created setup file that will create a database file

---
+++ src/bot/setup.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+f = open("src/bot/members.db", "w")
+f.close()
+
+conn = sqlite3.connect("src/bot/members.db")
+
+c = conn.cursor()
+c.execute("""CREATE TABLE users (
+          id INTEGER PRIMARY KEY,
+          token TEXT
+)""")
+conn.commit()
+conn.close()
