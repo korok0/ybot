a7f7a4c89fb4d98fdcb21fbcba159b55680550b0
korok <leafkorokleaf@gmail.com>
2023-09-11 21:38:11 -0400

    set logs command and added image support for log

--- src/cogs/message.py
+++ src/cogs/message.py
@@ -11,23 +11,46 @@ sys.path.append(project_root)
 class MessageCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.log_channel = None
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("message cog loaded!")
     
     @commands.Cog.listener()
-    async def on_message_delete(self, message):
+    async def on_message_delete(self, message: discord.Message):
         memb = message.author
-        channel = self.bot.get_channel(1130593170053398581)
-        mlog = f"Message sent by <@{memb.id}> deleted in <#{message.channel.id}>\n**Contents:** {message.content}"
-        embed = discord.Embed(description = mlog, color = discord.Colour.random())
-        embed.set_author(name=memb.name,icon_url=message.author.avatar)
+        if self.log_channel is not None:
+            
+            channel = self.log_channel
+        else:
+            channel = message.guild.owner
+            await channel.send("Logs will be sent to owner DM until log channel is set")
+        att = message.attachments
+        embed = discord.Embed(description=f"Message sent by <@{memb.id}> deleted in <#{message.channel.id}>\n**Contents:** {message.content}",
+                              color = discord.Colour.random()).set_author(name=memb.name, icon_url=message.author.avatar)
+        
+        # check if attatchment list is empty
+        if len(att) != 0:
+            embed.set_image(url=att[0].proxy_url)
         await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         pass
+    
+    @app_commands.checks.has_permissions(administrator=True, manage_channels=True)
+    @app_commands.command(name="setlogs", description="sets message log channel")
+    
+    async def set_logs(self, interaction: discord.Interaction):
+        self.log_channel = interaction.channel
+        await interaction.response.send_message(content="Log channel set!")
+
+    @set_logs.error
+    async def set_logs_error(self, interaction: discord.Interaction, error: discord.app_commands.MissingPermissions):
+        await interaction.response.send_message(embed=discord.Embed(
+            description=f"missing permissions: **{error.missing_permissions}**"), 
+            ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(MessageCog(bot))
